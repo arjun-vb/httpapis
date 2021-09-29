a218b45d8daae4ea374fd0fd1e5e56cb77475dca
Arjun <91302343+arjun-vb@users.noreply.github.com>
2021-09-28 19:28:50 -0700

    Update function.rb

--- function.rb
+++ function.rb
@@ -50,9 +50,12 @@ def main(event:, context:)
       if autharray.length() == 2 and autharray[0] == 'Bearer' and autharray[1] != nil and autharray[1].length > 0
         begin
           decodeToken = JWT.decode autharray[1], ENV['JWT_SECRET'], 'HS256'
-          response(body: decodeToken[0]['data'], status: 200)
+          respBody = decodeToken[0]['data']
+          response(body: respBody, status: 200)
         rescue JWT::ExpiredSignature, JWT::ImmatureSignature
           response(body: nil, status: 401)
+        rescue JWT::DecodeError, JWT::VerificationError
+          response(body: nil, status: 403)
         end
       else
         response(body: nil, status: 403)
